d1a0cf0414803c44f5ca8a73adcda7069ea95480
Ondřej Ticháček <ondrejtichacek@gmail.com>
2016-08-29 08:35:32 +0200

    fixup! Parsing and web output

--- web/parse_stats.rb
+++ web/parse_stats.rb
@@ -8,13 +8,17 @@ end
 
 def cluster_summary(name)
 
-	mtime = File.mtime("#{name}-qstat.txt")
+	mtime_qstat = File.mtime("#{name}-qstat.txt")
 
 	#QSTAT
 	scores = File.readlines("#{name}-qstat.txt")\
 		.select.with_index{|l,i| (1) != i}\
 		.map{|l| l.split(/\s+/)}
 	headers = scores.shift
+
+	dest_folder = "../data/pages/stats/"
+	contents = File.read("#{name}-qstat.txt")
+        File.write("../data/pages/stats/#{name}-qstat.txt", "<Code>\n"+contents+"</Code>")        
 
 	#remove first element
 	headers.shift
@@ -25,6 +29,9 @@ def cluster_summary(name)
 	sum_total = scores.map{|s| s['TOTAL'].to_i}.reduce(0, :+)
 
 	perc_avail = sum_avail.to_f/sum_total.to_f*100
+
+
+	mtime_df = File.mtime("#{name}-df.txt")
 
 	# FILESYSTEM
 	fs = File.readlines("#{name}-df.txt")\
@@ -37,11 +44,11 @@ def cluster_summary(name)
 	s_total = fs[0]['Size']#.reduce(0, :+)
 	p_avail = 100-fs[0]['Use%'].gsub!('%','').to_i
 
-return sum_avail, sum_total, perc_avail, s_avail, s_total, p_avail, mtime
+return sum_avail, sum_total, perc_avail, s_avail, s_total, p_avail, mtime_qstat, mtime_df
 end
 
 def cluster_summary_to_table_row(name)
-	sum_avail, sum_total, perc_avail, s_avail, s_total, p_avail, mtime = cluster_summary(name)
+	sum_avail, sum_total, perc_avail, s_avail, s_total, p_avail, mtime_qstat, mtime_df = cluster_summary(name)
 	
 	if p_avail >= 40
 		space_color = "@lightgreen:"
@@ -55,8 +62,10 @@ def cluster_summary_to_table_row(name)
 		core_color = "@pink:"
 	end
 
-	row = "^ <abbr>#{name}[Updated #{mtime}]</abbr>  |#{core_color} #{sum_avail}/#{sum_total} (#{perc_avail.to_i} %)  |#{space_color} #{s_avail} / #{s_total} (#{p_avail} %)  |\n"
+	row = "^ [[computing:#{name}|#{name}]] |  #{core_color} #{sum_avail}/#{sum_total} (#{perc_avail.to_i} %)  <abbr>\u25F7[Updated #{mtime_qstat}]</abbr>|  #{space_color} #{s_avail} / #{s_total} (#{p_avail} %) <abbr title='Updated #{mtime_df}'>\u25F7</abbr>|\n".encode('utf-8')
 end
+
+
 
 
 out = \
@@ -67,3 +76,4 @@ out = \
 #	"<fs 85%><color #AAAAAA>" + Time.now.utc.iso8601 + "</color></fs>\n"
 
 File.write('../data/pages/stats/summary.txt', out)
+
