e8f3eea926473a461b99c876a237bfa08ad59e86
Ondřej Ticháček <ondrejtichacek@gmail.com>
2016-09-27 19:20:16 +0200

    New colors and other appearance settings

--- web/graph/app.js
+++ web/graph/app.js
@@ -2,7 +2,10 @@ $(document).ready(function(){
 	$.ajax({
 		url: "api.php",
 		method: "GET",
-		data: { graph : 'clusters' },
+		data: {
+			//graph : 'clusters'
+			graph : 'queues'
+		},
 		success: function(data) {
 			console.log(data);
 
@@ -30,6 +33,10 @@ $(document).ready(function(){
 											 + (opacity || '.3') + ')';
 			};
 
+			var title_text = {
+				clusters: 'Free cores clusters',
+				queues: 'Free cores ratio queues'
+			};
 
 			var config = {
 					type: 'line',
@@ -38,7 +45,7 @@ $(document).ready(function(){
 							responsive: true,
 							title:{
 									display:true,
-									text:'Chart.js Line Chart'
+							 		text: title_text['clusters']
 							},
 							tooltips: {
 									mode: 'label',
@@ -57,12 +64,45 @@ $(document).ready(function(){
 			};
 
 			$.each(config.data.datasets, function(i, dataset) {
-					hex = colorbrewer['Paired'][config.data.datasets.length][i];
-					dataset.borderColor = hexToRgba(hex,0.4);
+				  //cmap = ['#8b0000', '#b61d39', '#d84765', '#ef738b', '#fea0ac', '#ffd1c9', '#ffffe0', '#c7f0ba', '#9edba4', '#7ac696', '#5aaf8c', '#399785','#008080']
+					//hex = cmap[i];
+					//hex = colorbrewer['Paired'][config.data.datasets.length][i];
+
+					var colors0 = ['darkred', 'deeppink', 'lightyellow'],
+					    colors1 = ['lightyellow', 'lightgreen', 'teal'];
+
+					// initialize chroma.scale
+	        var colors0 = chroma.bezier(colors0),
+	            colors1 = chroma.bezier(colors1);
+
+					var cs0 = chroma.scale(colors0).mode('lab').correctLightness(),
+              cs1 = chroma.scale(colors1).mode('lab').correctLightness();
+
+					var cs = function(t) {
+	            if (t < 0.5) return cs0(t*2);
+            	return cs1(t*2-1);
+	        };
+
+					hex = cs(i/config.data.datasets.length).hex();
+
+					dataset.borderWidth = 3;
+					//dataset.borderDash = [20,10];
+					dataset.borderColor = hexToRgba(hex,1);
+
 					//dataset.backgroundColor = hexToRgba(hex,0.3);
-					dataset.pointBorderColor = hexToRgba(hex,0.9);
-					dataset.pointBackgroundColor = hexToRgba(hex,0.5);
+					dataset.fill = false;
+
+					dataset.steppedLine = true;
+					dataset.pointRadius = 6;
+
+					//dataset.pointStyle = 'rect';
+
+					dataset.pointBorderColor = hexToRgba(hex,1);
+					dataset.pointBackgroundColor = hexToRgba(hex,1);
 					dataset.pointBorderWidth = 1;
+
+					dataset.pointHoverBackgroundColor = hexToRgba(hex,1);
+					dataset.pointHoverBorderColor = 'black';
 			});
 
 			var ctx = $("#mycanvas");
